524c222b3f545144be45ae978b56927f3759dce9
badshasha <shavendragoesoft@gmail.com>
2022-03-06 08:23:57 -0800

    one to one

---
+++ one_to_one(origi).cs
@@ -0,0 +1,18 @@
+public class Couch{
+
+    public int Id {get; set;}
+    public string Name {get ;set;}
+
+    public int? TeamId {get ;set;} // [+]   
+
+}
+
+
+public class Team{
+
+    
+    public int Id {get; set;}
+    public string Name {get ;set;}
+    public virtual Couch Couch {get; set;}
+
+}
