41b1dc7d94a6f4dcdcc8eec9e41203c66765c40e
badshasha <shavendragoesoft@gmail.com>
2022-03-06 07:44:24 -0800

    filter where EF functions

--- entityCommand.cs
+++ entityCommand.cs
@@ -20,3 +20,20 @@ await _context.SaveChangesAsync();
 var publisher = await _context.publishers.FindAsync(id);
 _context.publishers.Remove(publisher);
 await _context.SaveChangesAsync();
+
+
+
+// most common usage 
+
+
+            var p2  = _ad.publishers.First( x => x.Description.Equals("sri lanka"));
+            var p3 = _ad.publishers.Where(publisher => publisher.Name.Contains("s"));
+            var p4 = _ad.publishers.Where(publisher => EF.Functions.Like(publisher.Name,"%a%"));
+
+
+
+            
+            var count = _ad.publishers.Count();
+            var longCount = _ad.publishers.LongCount();
+            var min = _ad.publishers.MinAsync();
+            var max = _ad.publishers.MaxAsync();
