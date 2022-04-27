cc468eede90875cbc454aae4b1d05cf00fbf5ba1
badshasha <shavendragoesoft@gmail.com>
2022-04-27 04:52:34 -0700

    add customer ActionResult return

--- returntyle.cs
+++ returntyle.cs
@@ -8,3 +8,45 @@ specific
     only T
 
 
+/// cretae custom action result 
+
+
+/// add new view model 
+
+public class customActionResultVm{
+    public Exception  Exception{ get ; set;}
+    public object Data {get ;set;}   // if you need to specific type ( public Publisher data {get ;set;})
+
+}
+
+
+// nwe folder action result 
+
+public class CustomActionResult: IActionResult {
+
+    private readonly customActionResultVm _result;
+
+    public CustomActionResult(customActionResultVm result)
+    {
+        _result = result;
+    }
+
+    public async Task ExecuteResultAsync(ActionContext context){
+        // throw new Not impolementedException();
+
+        var objResult = new ObjectResult(_result.Exception ?? _result.pulisher as object ){
+            statusCoed = _result.Exception != null ? statusCode.Status500InternalServerError : statusCOde.status200ok        
+        };
+
+        await objectResult.ExecuteResultAsync( context );
+
+    }
+
+}
+
+// hwo to create responce onbject 
+
+var _response = new customActionResultVm(){
+    publiserh = responce 
+};
+return new CustomActionResult(_response);
