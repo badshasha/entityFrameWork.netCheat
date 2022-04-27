6862c718e835d2fb92df319e52d6778098d2046f
badshasha <shavendragoesoft@gmail.com>
2022-04-26 18:51:19 -0700

    add error status

---
+++ ErrorStatus.cs
@@ -0,0 +1,11 @@
+// this information ganaration from the system after it process correctly 
+
+//  201
+ return Created(nameof(AddBook),"book add success");
+ return Created(nameof(AddBook) , bookObj);
+
+
+// 
+return NotFound("error id please check your id");
+
+https://restfulapi.net/http-status-codes/ got to this link for additional information 
